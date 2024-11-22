1bdef9abafccac88223565cae980bd2ad176a0f2
Vishesh Konduru <visheshkonduru@gmail.com>
2024-11-22 14:30:58 -0500

    test

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'React'
+import React from 'React';
 import {Button} from "@/components/ui/Button";
 function Header() {
     return (
@@ -9,7 +9,6 @@ function Header() {
                 <div className={`flex items-center`}>
                     <h1 className={`font-black text-xl text-blue-600 p-5`}>Tables4u</h1>
                 </div>
-
                 {/* Right Side */}
                 <div className={`flex items-center gap-4`}>
                     <Button variant={"default"} className={`font-black m-2`}> CONSUMERS </Button>
