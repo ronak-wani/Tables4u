c29e82b1b445c3d7182f7d76e1c458dbecd90811
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2024-11-22 13:36:02 -0500

    Setup database connection

---
+++ lambda/deleteRestaurant/index.mjs
@@ -0,0 +1,8 @@
+import mysql from 'mysql'
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+});
