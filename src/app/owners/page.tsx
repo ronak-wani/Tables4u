f35ab6bda9e4f7cc62bad8f44b45ffccab06d949
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2024-11-22 13:28:31 -0500

    Created index.mjs

--- src/app/owners/page.tsx
+++ src/app/owners/page.tsx
@@ -4,7 +4,7 @@ export default function owner(){
     return(
         <>
             <div className={`flex items-center justify-end font-black m-5`}>
-                <Button>Create Restaurant</Button>
+                <Button onClick={}>Create Restaurant</Button>
             </div>
         </>
     )
