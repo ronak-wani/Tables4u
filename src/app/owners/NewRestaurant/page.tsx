0d9788d85f682fafc3c1a6a21329bb7d59492e48
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2024-11-22 22:03:41 -0500

    Login Functionality Complete

--- src/app/owners/NewRestaurant/page.tsx
+++ src/app/owners/NewRestaurant/page.tsx
@@ -5,11 +5,15 @@ import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/Button";
 import Link from "next/link";
+import { Copy } from 'lucide-react';
+
 
 export default function createRestaurantPage() {
     const [Name, setName] = React.useState("");
     const [Address, setAddress] = React.useState("");
     const [numberOfTables, setNumberOfTables] = React.useState(0);
+    const [isNotInvisible, setNotInvisible] = React.useState(true);
+    const [passcode, setPasscode] = React.useState("");
 
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setName(e.target.value);
@@ -22,6 +26,12 @@ export default function createRestaurantPage() {
     const handleNumberOfTablesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNumberOfTables(Number(e.target.value));
     };
+
+    function handleCreate(){
+        setPasscode((Math.random() * 999999).toString(36).slice(0));
+        console.log(passcode);
+        setNotInvisible(false);
+    }
     return (
         <>
             <div className="flex justify-between items-center m-5">
@@ -29,6 +39,7 @@ export default function createRestaurantPage() {
                     <Button>Back</Button>
                 </Link>
             </div>
+            {isNotInvisible && (
             <div className={`flex justify-center items-center h-full mt-44`}>
                 <Card className="w-[800px]">
                     <CardHeader>
@@ -50,12 +61,32 @@ export default function createRestaurantPage() {
                     </CardContent>
                     <CardFooter className="flex justify-between">
                         <Button variant="outline">Cancel</Button>
-                        <Link href="/owners/NewRestaurant/EditRestaurant">
-                            <Button>Create</Button>
-                        </Link>
+                        <Button onClick={() => handleCreate()}>Create</Button>
                     </CardFooter>
                 </Card>
-            </div>
+            </div>)}
+            {!isNotInvisible && (
+                <div className={`flex justify-center items-center h-full mt-44`}>
+                    <Card className="w-[800px]">
+                        <CardHeader>
+                            <CardTitle>Access Code</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-5">
+                                        <Label htmlFor="passcode">ACCESS KEY: {passcode} </Label>
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="text-gray-500 flex justify-between">
+                            <Label htmlFor="passcode">Remember to save the access key to edit and activate the restaurant </Label>
+                            <Button><Copy />Copy</Button>
+                        </CardFooter>
+
+                    </Card>
+                </div>)}
         </>
     )
 }
