e91eab037397e8382b903f25834fb99a84b2b42e
Eturas314 <144499966+Eturas314@users.noreply.github.com>
2024-11-26 16:13:41 -0500

    Added Lambda functions for loginAdministrator and adminViewRestaurants

---
+++ lambda/loginAdministrator/index.mjs
@@ -0,0 +1,43 @@
+import mysql from 'mysql';
+
+export const handler = async (event) => {
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+})
+    let response = {}
+
+    let ListRestauraunts = () => {
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT name, address, isActive, openHour, closeHour, numberOfTables FROM Restaurants", (error, rows) => {
+                if (error) { return reject(error); }
+                return resolve(rows);
+            })
+        })
+    }
+    if(event.adminPass === process.env.ADMIN_PASS){
+
+    
+    try{
+    const all_restaurants = await ListRestauraunts()
+    response = {
+        statusCode: 200,
+        restaurants: all_restaurants,
+        
+      }
+    }
+    catch(err) {
+        response = {statusCode: 400, error: err}
+    }
+}
+else {
+    response = {statusCode: 400, error: "Incorrect Admin Password"}
+}
+
+  
+
+pool.end()
+    return response;
+}
