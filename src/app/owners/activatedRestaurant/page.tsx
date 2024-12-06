63d6264b8ad82987e071ff3db3cc7346ab7b975c
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2024-12-06 15:52:00 -0500

    Update page.tsx

--- src/app/owners/activatedRestaurant/page.tsx
+++ src/app/owners/activatedRestaurant/page.tsx
@@ -4,9 +4,9 @@ import {Input} from "@/components/ui/input";
 import React, {useState} from "react";
 import DateCalendar from "@/app/(components)/Date";
 import Header from "@/app/(components)/Header";
-import {Button} from "react-day-picker";
 import {Dialog, DialogContent, DialogDescription, DialogTitle} from "@/components/ui/dialog";
 import axios from "axios";
+import {Button} from "@/components/ui/Button";
 
 const instance = axios.create({
         baseURL: 'https://8ng83lxa6k.execute-api.us-east-1.amazonaws.com/G2Iteration1'
@@ -60,49 +60,52 @@ export default function activateRestaurantPage() {
     return (
         <>
             <Header hidden={false}/>
-            <div className={`flex justify-center items-center h-full mt-44`}>
-                <Label htmlFor="closeDay" className={`mr-5`}>Close Future Date</Label>
-                <div className={`mr-5`}>
-                    <DateCalendar selectedDate={closeDay} onDateChange={handleCloseDay}
-                                  label="Select Close Date"/>
-                </div>
-                <Label htmlFor="openDay" className={`mr-5`}>Open Future Date</Label>
-                <div className={`mr-5`}>
-                    <DateCalendar selectedDate={openDay} onDateChange={handleOpenDay}
-                                  label="Select Open Date"/>
-                </div>
-                <Dialog open={saveDialogOpen} onOpenChange={setSaveDialogOpen}>
-                    <DialogContent>
-                        <DialogTitle>Are you absolutely sure?</DialogTitle>
-                        <DialogDescription>
-                            Customers will not be able to book reservations for the closed days
-                        </DialogDescription>
-                        <div className="flex justify-end space-x-2">
-                            <Input type="password" className={`w-1/2`} id="password"
-                                   placeholder="Access key" onChange={handleAccessKey}
-                                   required={true}/>
-                            <button
-                                className="px-4 py-2 bg-gray-200 rounded"
-                                onClick={() => {
-                                    handleSave(false); // Reset the switch if the user cancels
-                                    setSaveDialogOpen(false);
-                                }}
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                className="px-4 py-2 bg-blue-600 text-white rounded"
-                                onClick={() => {
-                                    handleSave(true); // Confirm the activation
-                                    setSaveDialogOpen(false);
-                                }}
-                            >
-                                Confirm
-                            </button>
+                <div className={`flex justify-center items-center h-full mt-44`}>
+                        <Label htmlFor="closeDay" className={`mr-5`}>Close Future Date</Label>
+                        <div className={`mr-5`}>
+                                <DateCalendar selectedDate={closeDay} onDateChange={handleCloseDay}
+                                              label="Select Close Date"/>
                         </div>
-                    </DialogContent>
-                </Dialog>
-            </div>
+                        <Label htmlFor="openDay" className={`mr-5`}>Open Future Date</Label>
+                        <div className={`mr-5`}>
+                                <DateCalendar selectedDate={openDay} onDateChange={handleOpenDay}
+                                              label="Select Open Date"/>
+                        </div>
+                        <div className="flex justify-center space-x-2">
+                                <Button type="button" onClick={() => setSaveDialogOpen(true)}>Save</Button>
+                                <Dialog open={saveDialogOpen} onOpenChange={setSaveDialogOpen}>
+                                        <DialogContent>
+                                                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                                                <DialogDescription>
+                                                        Customers will not be able to book reservations for the closed days
+                                                </DialogDescription>
+                                                <div className="flex justify-end space-x-2">
+                                                        <Input type="password" className={`w-1/2`} id="password"
+                                                               placeholder="Access key" onChange={handleAccessKey}
+                                                               required={true}/>
+                                                        <button
+                                                            className="px-4 py-2 bg-gray-200 rounded"
+                                                            onClick={() => {
+                                                                    handleSave(false); // Reset the switch if the user cancels
+                                                                    setSaveDialogOpen(false);
+                                                            }}
+                                                        >
+                                                                Cancel
+                                                        </button>
+                                                        <button
+                                                            className="px-4 py-2 bg-blue-600 text-white rounded"
+                                                            onClick={() => {
+                                                                    handleSave(true); // Confirm the activation
+                                                                    setSaveDialogOpen(false);
+                                                            }}
+                                                        >
+                                                                Confirm
+                                                        </button>
+                                                </div>
+                                        </DialogContent>
+                                </Dialog>
+                        </div>
+                </div>
         </>
     )
 };
